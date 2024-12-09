1f3b29184fddfbe96de71b831b141bc6c062829e
Lucius Artorius <superuser0056@gmail.com>
2024-12-09 23:36:11 +0500

    update date

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ mongoose
 // Создание схемы и модели для заметок
 const noteSchema = new mongoose.Schema({
   content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
 const Note = mongoose.model("Note", noteSchema);
